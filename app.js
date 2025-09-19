25fe6b272c0a8f2dc184cdc341c7ea958d3d07e3
hafizhussainlodhi <hasanlodhi000@gmail.com>
2025-09-19 02:16:55 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,62 @@
+// ===========================
+// Chapter 2 - VARIABLES FOR STRINGS
+// ===========================
+// Part 1
+// Declare a variable called username
+var username;
+
+// --------------------
+// Part 2
+// Declare a variable myName and assign your full name
+var myName = "Hussain Ali";
+
+// --------------------
+// Part 3
+// Declare message variable, assign "Hello World" and show in alert
+var message;
+message = "Hello World";
+alert(message);
+
+// --------------------
+// Part 4
+// Save student’s bio data in variables and show in alert boxes
+var studentName = "Hussain";
+var studentAge = "18 years old";
+var studentClass = "12th Grade";
+var studentCourse = "Web Development";
+
+alert("Name: " + studentName);
+alert("Age: " + studentAge);
+alert("Class: " + studentClass);
+alert("Course: " + studentCourse);
+
+// --------------------
+// Part 5
+// Display alert using only one variable
+var foodMessage = "Pizza\nPizz\nPiz\nPi\nP";
+alert(foodMessage);
+
+// --------------------
+// Part 6
+// Email variable with concatenation
+var email = "hussain123@gmail.com";
+alert("My email address is: " + email);
+
+// --------------------
+// Part 7
+// Book variable with value
+var book = "A smarter way to learn JavaScript";
+alert("I am trying to learn from the book " + book);
+
+// --------------------
+// Part 8
+// Display message in browser
+document.write("This message is displayed directly in the browser.<br>");
+
+// --------------------
+// Part 9
+// Store string in variable and show in alert and browser
+var message2 = "Thanks for visiting!"
+alert(message2);
+document.write(message2);
+
